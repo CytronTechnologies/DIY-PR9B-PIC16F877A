1732ecf7a0920e84e74e4d0309f4a55358c7320e
CytronTrainee <cytontrainee@gmail.com>
2015-07-13 14:27:25 +0800

    DIY PR9B
    DIY PR9B sample code (old).

---
+++ PR9B MPLABX/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=PR_9B___P.D.S_V2010.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/PR_9B___P.D.S_V2010.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=pr9bp.d.sv2010.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/pr9bp.d.sv2010.x.tar
